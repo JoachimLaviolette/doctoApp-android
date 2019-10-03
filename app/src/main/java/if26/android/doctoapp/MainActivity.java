111faf3fec500204ce803974ce304c4a28279008
Joachim Laviolette <joachim.laviolette@gmail.com>
2019-10-03 17:21:50 +0200

    Created Login activity and linked it to Main activity.

--- app/src/main/java/if26/android/doctoapp/MainActivity.java
+++ app/src/main/java/if26/android/doctoapp/MainActivity.java
@@ -2,13 +2,49 @@ package if26.android.doctoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity
+        implements View.OnClickListener {
+
+    private ImageButton loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.Instantiate();
+        this.SubscribeEvents();
+
+    }
+
+    private void Instantiate() {
+        this.loginBtn = findViewById(R.id.main_login);
+    }
+
+    private void SubscribeEvents() {
+        this.loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_login:
+                this.Login();
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void Login() {
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
     }
 }
