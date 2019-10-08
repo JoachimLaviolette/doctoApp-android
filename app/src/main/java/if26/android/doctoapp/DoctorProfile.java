0df924059dbccc751a3fa15f3b509fe82eab2580
Joachim Laviolette <joachim.laviolette@gmail.com>
2019-10-08 17:57:48 +0200

    Fixed app running bug with gradle file. Created doctor profile popup sample. Data retrieving when touching one section working. Still have to debug data display inside the popup.

--- app/src/main/java/if26/android/doctoapp/DoctorProfile.java
+++ app/src/main/java/if26/android/doctoapp/DoctorProfile.java
@@ -1,24 +1,50 @@
 package if26.android.doctoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class DoctorProfile
         extends AppCompatActivity
-        implements View.OnClickListener {
+        implements View.OnClickListener, View.OnTouchListener {
 
+    private ConstraintLayout mainLayout;
     private Button bookAppointmentBtn;
     private ImageView doctorPicture;
     private TextView
-            doctorFullname,
-            doctorSpeciality,
-            doctorAddress;
+                doctorFullname,
+                doctorSpeciality;
+    private TextView
+                doctorAddressContent,
+                doctorPricesRefundsContent,
+                doctorPaymentOptionsContent,
+                doctorDescriptionContent,
+                doctorHoursContactsContent,
+                doctorEducationContent,
+                doctorLanguagesContent,
+                doctorExperiencesContent;
+    private LinearLayout
+            doctorAddressSection,
+            doctorPricesRefundsSection,
+            doctorPaymentOptionsSection,
+            doctorDescriptionSection,
+            doctorHoursContactsSection,
+            doctorEducationSection,
+            doctorLanguagesSection,
+            doctorExperiencesSection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +60,31 @@ public class DoctorProfile
      * Retrieve the view components references
      */
     private void Instantiate() {
+        this.mainLayout = findViewById(R.id.doctor_profile_layout);
         this.bookAppointmentBtn = findViewById(R.id.doctor_profile_book_appointment);
         this.doctorPicture = findViewById(R.id.doctor_profile_picture);
         this.doctorFullname = findViewById(R.id.doctor_profile_fullname);
         this.doctorSpeciality = findViewById(R.id.doctor_profile_speciality);
-        this.doctorAddress = findViewById(R.id.doctor_profile_address_content);
+
+        // Sections
+        this.doctorAddressSection = findViewById(R.id.doctor_profile_address_section);
+        this.doctorPricesRefundsSection = findViewById(R.id.doctor_profile_prices_refunds_section);
+        this.doctorPaymentOptionsSection = findViewById(R.id.doctor_profile_payment_options_section);
+        this.doctorDescriptionSection = findViewById(R.id.doctor_profile_description_section);
+        this.doctorHoursContactsSection = findViewById(R.id.doctor_profile_hours_contacts_section);
+        this.doctorEducationSection = findViewById(R.id.doctor_profile_education_section);
+        this.doctorLanguagesSection = findViewById(R.id.doctor_profile_languages_section);
+        this.doctorExperiencesSection = findViewById(R.id.doctor_profile_experiences_section);
+
+        // Content
+        this.doctorAddressContent = findViewById(R.id.doctor_profile_address_content);
+        this.doctorPricesRefundsContent = findViewById(R.id.doctor_profile_prices_refunds_content);
+        this.doctorPaymentOptionsContent = findViewById(R.id.doctor_profile_payment_options_content);
+        this.doctorDescriptionContent = findViewById(R.id.doctor_profile_description_content);
+        this.doctorHoursContactsContent = findViewById(R.id.doctor_profile_hours_contacts_content);
+        this.doctorEducationContent = findViewById(R.id.doctor_profile_education_content);
+        this.doctorLanguagesContent = findViewById(R.id.doctor_profile_languages_content);
+        this.doctorExperiencesContent = findViewById(R.id.doctor_profile_experiences_content);
     }
 
     /**
@@ -59,7 +105,7 @@ public class DoctorProfile
         this.doctorSpeciality.setText(doctorService.GetDoctorSpeciality(doctor));
 
         // Set doctor address
-        this.doctorAddress.setText(doctorService.GetDoctorAddress(doctor));
+        this.doctorAddressContent.setText(doctorService.GetDoctorAddress(doctor));
     }
 
     /**
@@ -67,6 +113,14 @@ public class DoctorProfile
      */
     private void SubscribeEvents() {
         this.bookAppointmentBtn.setOnClickListener(this);
+        this.doctorAddressSection.setOnTouchListener(this);
+        this.doctorPricesRefundsSection.setOnTouchListener(this);
+        this.doctorPaymentOptionsSection.setOnTouchListener(this);
+        this.doctorDescriptionSection.setOnTouchListener(this);
+        this.doctorHoursContactsSection.setOnTouchListener(this);
+        this.doctorEducationSection.setOnTouchListener(this);
+        this.doctorLanguagesSection.setOnTouchListener(this);
+        this.doctorExperiencesSection.setOnTouchListener(this);
     }
 
     /**
@@ -75,12 +129,65 @@ public class DoctorProfile
      */
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.doctor_profile_book_appointment:
-                this.BookAppointment();
+        if (v.getId() == R.id.doctor_profile_book_appointment) this.BookAppointment();
+    }
 
-                return;
+    /**
+     * Handle touch events
+     * @param v The Main view
+     */
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            int titleKey, contentKey;
+
+            switch (v.getId()) {
+                case R.id.doctor_profile_address_section:
+                    titleKey = R.string.doctor_profile_address;
+                    contentKey = R.id.doctor_profile_address_content;
+                    break;
+                case R.id.doctor_profile_description_section:
+                    titleKey = R.string.doctor_profile_description;
+                    contentKey = R.id.doctor_profile_description_content;
+                    break;
+                case R.id.doctor_profile_education_section:
+                    titleKey = R.string.doctor_profile_education;
+                    contentKey = R.id.doctor_profile_education_content;
+                    break;
+                case R.id.doctor_profile_experiences_section:
+                    titleKey = R.string.doctor_profile_experiences;
+                    contentKey = R.id.doctor_profile_experiences_content;
+                    break;
+                case R.id.doctor_profile_hours_contacts_section:
+                    titleKey = R.string.doctor_profile_hours_contacts;
+                    contentKey = R.id.doctor_profile_hours_contacts_content;
+                    break;
+                case R.id.doctor_profile_languages_section:
+                    titleKey = R.string.doctor_profile_languages;
+                    contentKey = R.id.doctor_profile_languages_content;
+                    break;
+                case R.id.doctor_profile_payment_options_section:
+                    titleKey = R.string.doctor_profile_payment_options;
+                    contentKey = R.id.doctor_profile_payment_options_content;
+                    break;
+                case R.id.doctor_profile_prices_refunds_section:
+                    titleKey = R.string.doctor_profile_prices_refunds;
+                    contentKey = R.id.doctor_profile_prices_refunds_content;
+                    break;
+                default:
+                    titleKey = R.string.doctor_profile_popup_title;
+                    contentKey = R.string.doctor_profile_popup_content;
+            }
+
+            String title = this.getResources().getString(titleKey);
+            String content = ((TextView) findViewById(contentKey)).getText().toString();
+
+            this.CreatePopup(title, content);
+
+            return true;
         }
+
+        return false;
     }
 
     /**
@@ -88,5 +195,32 @@ public class DoctorProfile
      */
     private void BookAppointment() {
 
+    }
+
+    /**
+     * Open a new popup
+     * @param title The title of the popup
+     * @param content The content of the popup
+     */
+    private void CreatePopup(String title, String content) {
+        // Initialize a new instance of LayoutInflater service
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        // Inflate the popup layout
+        View popupView = inflater.inflate(R.layout.doctor_profile_popup_layout, null);
+
+        // Create the popup window
+        PopupWindow popup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+        // Display the popup
+        popup.showAtLocation(this.mainLayout, Gravity.CENTER, 0, 0);
+
+        // Retrieve popup components references
+        TextView popupTitle = findViewById(R.id.doctor_profile_popup_title);
+        TextView popupContent = findViewById(R.id.doctor_profile_popup_content);
+
+        // Set the popup title
+        popupTitle.setText(title);
+        popupContent.setText(content);
     }
 }
--- app/src/main/java/if26/android/doctoapp/MainActivity.java
+++ app/src/main/java/if26/android/doctoapp/MainActivity.java
@@ -1,7 +1,5 @@
 package if26.android.doctoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -9,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity
         extends AppCompatActivity
